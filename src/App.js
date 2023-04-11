26ae632fa862e41093df4c6f72fd597ac39e3552
Saw-YanLinOo <yly80103@gmail.com>
2023-04-11 18:34:48 +0630

    ✨ search github users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = "https://api.github.com/users";
+const searchUrl = "https://api.github.com/search/users?q=";
+
+function App(){
+    const [inputValue,setInputValue] = useState("");
+    const [users,setUsers] = useState([]);
+    const [isLoading,setIsLoading] = useState(true);
+    const [isError,setIsError] = useState(false);
+
+    async function getUsers(){
+
+        var response = await fetch(url);
+        var users = await response.json();
+
+        if(response.status > 300){
+            setIsError(true);
+            setIsLoading(false);
+        }
+        setIsLoading(false);
+        setUsers(users);
+        
+    }
+
+    async function searchusers(userName){
+        var response = await fetch(searchUrl+userName);
+        var users = await response.json();
+
+        if(response.status > 300){
+            setIsError(true);
+            setIsLoading(false);
+        }
+        setIsLoading(false);
+        setUsers(users.items);
+    }
+
+    function submitHandler(e){
+        e.preventDefault();
+        
+        searchusers(inputValue);
+    }
+
+    useEffect(()=>{
+        getUsers();
+    },[])
+
+
+    if(isLoading){
+        return <h1>Loading...</h1>
+    }
+
+    if(isError){
+        return <h1>Opps ...</h1>
+    }
+
+    return (
+        <div className="container">
+            <h1>Github Users</h1>
+            <form onSubmit={submitHandler}>
+                <input
+                type="text"
+                value={inputValue}
+                onChange={(e)=> setInputValue(e.target.value)}
+                placeholder="Search"
+                />
+                <button type="submit">Submit</button>
+            </form>
+            <ul className="users">
+                {
+                    users.map((user)=>{
+                        return <li key={user.id}>
+                            <img src={user.avatar_url} alt={user.login}/>
+                        <div>
+                            <h4>{user.login}</h4>
+                            <a href={user.html_url}>Profile</a>
+                        </div>
+                        </li>
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default App;
